483bd0070180f54261b7a768df1540a05dce9ddf
Pinzon <moisespinzon97@hotmail.com>
2024-12-11 11:14:59 -0500

    EntregaJava

---
+++ JavaSprintBoot-FullStack/src/main/java/com/PruebaTecnica/JavaSprintBoot_FullStack/JavaSprintBootFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.PruebaTecnica.JavaSprintBoot_FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprintBootFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprintBootFullStackApplication.class, args);
+	}
+
+}
---
+++ JavaSprintBoot-FullStack/src/test/java/com/PruebaTecnica/JavaSprintBoot_FullStack/JavaSprintBootFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaTecnica.JavaSprintBoot_FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprintBootFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
